a046f19ead805b648af79f20703e75ede2959c1d
boriur <91899401+b0riur@users.noreply.github.com>
2026-08-19 00:43:33 -0300

    Update Limpar-Temp.ps1

--- WinTools-ContextMenu/src/scripts/Limpar-Temp.ps1
+++ WinTools-ContextMenu/src/scripts/Limpar-Temp.ps1
@@ -1,1 +1,16 @@
+$UserTemp = $env:TEMP
+Stop-Service wuauserv -Force -ErrorAction SilentlyContinue
 
+$Caminhos = @(
+    "$UserTemp\*",
+    "C:\Windows\Temp\*",
+    "C:\Windows\SoftwareDistribution\Download\*",
+    "C:\Windows\Prefetch\*",
+    "C:\ProgramData\Microsoft\Windows\WER\ReportQueue\*"
+)
+
+foreach ($Caminho in $Caminhos) {
+    Remove-Item -Path $Caminho -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+Start-Service wuauserv -ErrorAction SilentlyContinue
