d0af928536aad4ed2d2f3023dd53eafd69070911
boriur <91899401+b0riur@users.noreply.github.com>
2026-08-19 00:45:57 -0300

    Add uninstall script for WinTools-ContextMenu

--- WinTools-ContextMenu/Uninstall.bat
+++ WinTools-ContextMenu/Uninstall.bat
@@ -1,1 +1,17 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    powershell -Command "Start-Process '%~0' -Verb RunAs"
+    exit /b
+)
 
+set TARGET_DIR=C:\ProgramData\WinTools
+
+echo Desinstalando WinTools-ContextMenu...
+reg delete "HKCR\Directory\Background\shell\Tools" /f >nul 2>&1
+reg delete "HKCR\Directory\Background\shell\Planos de Energia" /f >nul 2>&1
+
+if exist "%TARGET_DIR%" rmdir /s /q "%TARGET_DIR%"
+
+echo Desinstalacao concluida!
+pause
